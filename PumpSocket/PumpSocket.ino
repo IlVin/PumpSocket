2431f21f80b5c13c75b3a7cb209157134f9ffefd
Ilia Vinokurov <ilvin@mail.ru>
2021-08-15 15:38:17 +0300

    Added state machine

--- PumpSocket/PumpSocket.ino
+++ PumpSocket/PumpSocket.ino
@@ -1,60 +1,167 @@
 #include <inttypes.h>
-#include <LiquidCrystal.h> //библиотека для работы с дисплеем
+#include <LiquidCrystal.h>  // библиотека для работы с дисплеем
 #include <sstream>
 #include <string>
 #include <stdio.h>
 #include <cstdlib>
 
 
-//пины подключенные к реле//
+// пины подключенные к реле //
 #define R1 5
 #define R2 16
+
+// Тайминги
+#define ON_DURATION 30
+#define OFF_DURATION 200
+
+// Пробники тока и напряжения //
+long I1 = 0;
+long I2 = 0;
+long V1 = 0;
+
+// Данные для отображения на дисплее //
+std::string statusString;
+long sec;
+
+long last_I1 = 0;
+long last_I2 = 0;
+
 ///////////////////////////
 
-//настраиваем дисплей, указывая пины к которым подключен дисплей
+
+// настраиваем дисплей, указывая пины к которым подключен дисплей
 LiquidCrystal lcd { 4, 0, 2, 14, 12, 13 }; 
+char lcdBuffer [30];
+
+struct {
+  long startMs;
+  long diffMs;
+} ctx_wait;
+
+
+// Парсит показания пробников
+void parseString(std::string S){
+  if ((S[0] == 'R') && (S[1] == 'M') && (S[2] == 'S')){
+    switch(S[4]){
+      case '5': I2 = atol(std::string(S,7).c_str()); break;
+      case '6': I1 = atol(std::string(S,7).c_str()); break;
+      case '7': V1 = atol(std::string(S,7).c_str()); break;
+    }
+  }
+}
+
+// Cчитывает показания пробников из UART
+void readUART(){
+  static std::string S;
+  while (Serial.available()) {  // пока есть данные, читаем
+    char ch = (char)(Serial.read());
+    Serial.write(ch);
+    if (ch == '\n') {
+      parseString(S);
+      S = "";
+    } else {
+      S = S + ch;
+    }
+  }
+}
+
+// Выводит информацию на дисплей
+void showInfo() {
+    lcd.setCursor(0, 0);
+    long secs = sec % 60;
+    long mins = (sec / 60) % 60;
+    long hours = sec / 3600;
+    int n = sprintf(lcdBuffer, "%4s: %02d:%02d:%02d        ", statusString.c_str(), hours, mins, secs);
+    lcdBuffer[16] = '\0';
+    lcd.write(lcdBuffer);
+
+    lcd.setCursor(0, 1);
+    n = sprintf(lcdBuffer, "%d : %d           ", I1, I2);
+    lcdBuffer[16] = '\0';
+    lcd.write(lcdBuffer);
+}
+
+enum STATE { POWER_ON, WAIT_LOAD, WORK, POWER_OFF, PAUSE };
+
+STATE state;
+
+void state_power_on() {
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, HIGH);
+  lcd.begin(16, 2);
+  ctx_wait.startMs = millis();
+  state = WAIT_LOAD;
+}
+
+void state_wait_load() {
+  statusString = "WAIT";
+  ctx_wait.diffMs = millis() - ctx_wait.startMs;
+  sec = ctx_wait.diffMs / 1000;
+  if (I1 > 300 || I2 > 300) {
+    lcd.begin(16, 2);
+    last_I1 = I1;
+    last_I2 = I2;
+    ctx_wait.startMs = millis();
+    state = WORK;
+  }
+}
+
+void state_work() {
+  statusString = " ON ";
+  ctx_wait.diffMs = ctx_wait.startMs + 1000 * ON_DURATION - millis();
+  sec = ctx_wait.diffMs / 1000;
+  if (ctx_wait.diffMs <= 0) {
+    state = POWER_OFF;
+  }
+  // При засасывании воздуха насос начинает потреблять меньше тока
+  // При срабатывании реле давления насос перестает работать
+  if (last_I1 > I1 && last_I1 - I1 > 150) {
+    state = POWER_OFF;
+  }
+  if (last_I2 > I2 && last_I2 - I2 > 150) {
+    state = POWER_OFF;
+  }
+  // При повышении давления в системе насос потребляет больше тока
+  last_I1 = I1;
+  last_I2 = I2;
+}
+
+void state_power_off() {
+  digitalWrite(R1, HIGH);
+  digitalWrite(R2, LOW);
+  lcd.begin(16, 2);
+  ctx_wait.startMs = millis();
+  state = PAUSE;
+}
+
+void state_pause() {
+  statusString = "OFF ";
+  ctx_wait.diffMs = ctx_wait.startMs + 1000 * OFF_DURATION - millis();
+  sec = ctx_wait.diffMs / 1000;
+  if (ctx_wait.diffMs <= 0) {
+    state = POWER_ON;
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(R1, OUTPUT);
   pinMode(R2, OUTPUT);
-  
-  digitalWrite(R1, LOW);
-  digitalWrite(R2, LOW);
+
+  Serial.begin(19200, SERIAL_8N1);
 
   lcd.begin(16, 2);
+
+  state = POWER_ON;
 }
 
 void loop() {
-  int WORK_DURATION = 30;
-  int WAIT_DURATION = 180;
-  char lcdBuffer [30];
-
-  // Включаем розетку
-  digitalWrite(R1, HIGH);
-  digitalWrite(R2, HIGH);
-
-  // Рабочий цикл
-  lcd.begin(16, 2);
-  for (int sec = WORK_DURATION; sec > 0; sec--) {
-    int n = sprintf(lcdBuffer, "ON :%ds         ", sec);
-    lcdBuffer[16] = '\0';
-    lcd.setCursor(0, 0);
-    lcd.print(lcdBuffer);
-    delay(1000);
+  switch (state) {
+    case POWER_ON: state_power_on(); break;
+    case WAIT_LOAD: state_wait_load(); break;
+    case WORK: state_work(); break;
+    case POWER_OFF: state_power_off(); break;
+    case PAUSE: state_pause(); break;
   }
-
-  // Выключаем розетку
-  digitalWrite(R1, LOW);
-  digitalWrite(R2, LOW);
-
-  // Цикл ожидания
-  lcd.begin(16, 2);
-  for (int sec = WAIT_DURATION; sec > 0; sec--) {
-    int n = sprintf(lcdBuffer, "OFF:%ds         ", sec);
-    lcdBuffer[16] = '\0';
-    lcd.setCursor(0, 0);
-    lcd.print(lcdBuffer);
-    delay(1000);
-  }
+  readUART();
+  showInfo();
 }
